fa31c42096f26fb1be37a7520c2d7e6754c87c90
MasaViktoriya <vikoriya.masa@gmail.com>
2022-05-13 03:53:51 +0300

    ДЗ 3-3: логирование истории и показ 100 последних сообщений

--- src/main/java/com/geekbrains/client/ChatController.java
+++ src/main/java/com/geekbrains/client/ChatController.java
@@ -1,5 +1,5 @@
 package com.geekbrains.client;
-
+import com.geekbrains.server.ServerCommandConstants;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -7,14 +7,17 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.ListView;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextFlow;
-
+import java.io.*;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.ResourceBundle;
+
 
 public class ChatController implements Initializable {
     @FXML
@@ -29,12 +32,14 @@ public class ChatController implements Initializable {
     private ListView<String> clientList;
 
     private final Network network;
+    private File history;
+    private String login;
 
     public ChatController() {
         this.network = new Network(this);
     }
 
-    public void setAuthenticated(boolean authenticated){
+    public void setAuthenticated(boolean authenticated) {
         authPanel.setVisible(!authenticated);
         authPanel.setManaged(!authenticated);
         messagePanel.setVisible(authenticated);
@@ -52,18 +57,31 @@ public class ChatController implements Initializable {
         Platform.runLater(new Runnable() {
             @Override
             public void run() {
-                Text textNode = new Text(text);
-                if (text.startsWith("PM ")) {
-                    textNode.setFill(Color.DARKGREEN);
-                    textFlow.getChildren().add(textNode);
-                } else {
-                    textFlow.getChildren().add(textNode);
-                }
+                addTextNode(text);
+                logMessages(text);
             }
-            });
-        }
+        });
+    }
 
-    public void displayClient(String nickName){
+    public void addTextNode(String textForTextFlow) {
+        Text textNode = new Text(textForTextFlow);
+        if (textForTextFlow.startsWith("PM ")) {
+            textNode.setFill(Color.DARKGREEN);
+            textFlow.getChildren().add(textNode);
+        } else {
+            textFlow.getChildren().add(textNode);
+        }
+    }
+
+    private void logMessages(String text) {
+        try (OutputStreamWriter historyWriter = new OutputStreamWriter(new FileOutputStream(history, true), StandardCharsets.UTF_8)) {
+            historyWriter.write(text + "\r\n");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void displayClient(String nickName) {
         Platform.runLater(new Runnable() {
             @Override
             public void run() {
@@ -76,24 +94,52 @@ public class ChatController implements Initializable {
         Platform.runLater(new Runnable() {
             @Override
             public void run() {
-                clientList.getItems().remove(nickName);
+               clientList.getItems().remove(nickName);
             }
         });
     }
 
-    public boolean isNicknameInClientList (String oldNickName){
+    public boolean isNicknameInClientList(String oldNickName) {
         if (clientList.getItems().contains(oldNickName)) {
             return true;
-        };
+        }
         return false;
     }
 
     public void sendAuth(ActionEvent actionEvent) {
         boolean authenticated = network.sendAuth(loginField.getText(), passwordField.getText());
         if (authenticated) {
+            this.login = loginField.getText();
+            history = new File("history_" + login + ".txt");
             loginField.clear();
             passwordField.clear();
             setAuthenticated(true);
+            displayMessageHistory();
+        }
+    }
+
+    private void displayMessageHistory() {
+        ArrayList<String> historyList = new ArrayList<>();
+        if (new File("history_" + login + ".txt").isFile()) {
+            try (BufferedReader historyReader = new BufferedReader(new InputStreamReader(new FileInputStream(history), StandardCharsets.UTF_8))) {
+                while (historyReader.ready()) {
+                    historyList.add(historyReader.readLine());
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            String[] historyArray = new String[historyList.size()];
+            historyArray = historyList.toArray(historyArray);
+            if (historyArray.length > 200) {  // число 200 появилось, поскольку используется метод readLine, он считывает переносы строк как отдельные строки
+                String[] displayArray = Arrays.copyOfRange(historyArray, historyArray.length - 200, historyArray.length);
+                for (String text : displayArray) {
+                    addTextNode(text + "\n");
+                }
+            } else {
+                for (String text : historyArray) {
+                   addTextNode(text + "\n");
+                }
+            }
         }
     }
 
@@ -102,19 +148,30 @@ public class ChatController implements Initializable {
         if (selectedUser == null) {
             network.sendMessage(messageField.getText());
             messageField.clear();
-        }else {
-/*            clientList.addEventFilter(MouseEvent.MOUSE_CLICKED, event ->{
+        } else {
+/*    Проблемы по фронтенду
+Хотела очищать выделение адресата по клику на окошке с сообщениями,
+чтобы клиент мог передумать и не писать ЛС, но не знаю, как сделать. Метод ниже не работает.
+Сейчас, если выбрал адресата для ЛС, то придется отправить ему сообщение, иначе нельзя написать в общий чат.
+Также не нашла, как сделать отступы без использования CSS
+И хотелось бы прописывать логин в заголовке окна после аутентификации, но не получается обратиться к stage
+
+           clientList.addEventFilter(MouseEvent.MOUSE_CLICKED, event ->{
                 if(clientList.getSelectionModel().getSelectedIndices().get(0) <= -1){
                 clientList.getSelectionModel().clearSelection();
                 }
             });*/
-            network.sendMessage("/w " + selectedUser +" "+ messageField.getText());
+            network.sendMessage(ServerCommandConstants.PERSONALMESSAGE + selectedUser + " " + messageField.getText());
             messageField.clear();
             clientList.getSelectionModel().clearSelection();
         }
     }
 
-    public void close(){
+    public void close() {
         network.closeConnection();
+    }
+
+    public String getLogin() {
+        return login;
     }
 }
